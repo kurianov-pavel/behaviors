aab0676b5e263a6dafffd736a90847c377b6534e
Pavel Kurianov <kurianov.pavel@gmail.com>
2019-11-05 15:56:24 +0200

    Add DataHandlerBehavior with IsShown property

---
+++ Library/Behaviors/Handlers/DataHandlerBehavior.cs
@@ -0,0 +1,47 @@
+﻿using Xamarin.Forms;
+
+namespace Behaviors.Handlers
+{
+    [Preserve(AllMembers = true)]
+    public sealed class DataHandlerBehavior : BehaviorPropertiesBase
+    {
+        private VisualElement _associatedView;
+
+        public static readonly BindableProperty IsShownProperty =
+            BindableProperty.Create(
+                nameof(IsShown),
+                typeof(bool),
+                typeof(DataHandlerBehavior),
+                false,
+                propertyChanged: OnDataHandlerBehavior);
+        public bool IsShown
+        {
+            get => (bool)GetValue(IsShownProperty);
+            set => SetValue(IsShownProperty, value);
+        }
+        protected override void OnAttachedTo(VisualElement bindable)
+        {
+            _associatedView = bindable;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(VisualElement bindable)
+        {
+            _associatedView = null;
+            base.OnDetachingFrom(bindable);
+        }
+
+        static async void OnDataHandlerBehavior(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is DataHandlerBehavior behavior && newValue is bool val)
+            {
+                foreach (BindableObject bind in behavior.Actions)
+                {
+                    bind.BindingContext = behavior.BindingContext;
+                    var action = (IAction)bind;
+                    await action.Execute(behavior, null);
+                }
+            }
+        }
+    }
+}
